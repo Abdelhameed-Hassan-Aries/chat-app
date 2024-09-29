7c87a41dc5850c85ac93dbafd106432820d6246f
Abdelhameed Hassan <abdelhameed.hs0@gmail.com>
2024-09-29 10:35:54 +0300

    removed empty styling file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import "./App.css";
 import Chat from "./components/Chat";
 
 function App() {
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <div className="bg-gray-200 min-h-screen">
       <Chat />
     </div>
   );
