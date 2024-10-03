127e8a350596eb6cb47526117a70d1c82bbf2492
Abdelhameed Hassan <abdelhameed.hs0@gmail.com>
2024-10-03 13:38:24 +0300

    fixed styling issues

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -8,7 +8,7 @@ const Button = React.forwardRef(
       className={clsx(
         "inline-flex items-center justify-center w-8 h-8 p-0 m-1 text-sm font-medium rounded-full",
         disabled
-          ? "cursor-not-allowed opacity-50"
+          ? "opacity-50"
           : "bg-gray-200 dark:bg-gray-600 hover:bg-gray-300 dark:hover:bg-gray-500",
         "focus:outline-none",
         className
